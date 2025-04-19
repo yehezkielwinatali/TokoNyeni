b13f874657ce2f43e106e9db54f25c2963f056fd
joshuavg25 <joshuagunawan2004@gmail.com>
2025-04-19 17:26:27 +0900

    js edit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Script for navigation bar
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
