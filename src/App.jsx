639560dccc2f242e34c87a730467dc81e44bab1c
joshuavg25 <joshuagunawan2004@gmail.com>
2025-06-02 22:58:28 +0900

    josh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Shop from "./pages/shop";
+import About from "./pages/about";
+import Contact from "./pages/contact";
 
 function Placeholder({ name }) {
   return (
@@ -16,8 +18,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
       <Route path="/blog" element={<Placeholder name="Blog" />} />
-      <Route path="/about" element={<Placeholder name="About" />} />
-      <Route path="/contact" element={<Placeholder name="Contact" />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="/cart" element={<Placeholder name="Cart" />} />
     </Routes>
   );
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,22 +1,185 @@
-// <!DOCTYPE html>
-// <html lang="en">
+import { Link } from "react-router-dom";
+import "../styles/contact.css";
 
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//         <title>Document</title>
+function Contact() {
+  return (
+    <>
+      <section id="header">
+        <Link to="/" className="logo">
+          <img src="/img/logo.png" alt="logo" style={{ width: "90px" }} />
+        </Link>
+        <div>
+          <ul id="navbar">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/about">About </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="active">Contact</Link>
+            </li>
+            <li id="lg-bag">
+              <Link to="/cart" id="cart-icon">
+                <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+              </Link>
+            </li>
+            <a href="#" id="close">
+              <i className="far fa-times"></i>
+            </a>
+          </ul>
+        </div>
+        <div id="mobile">
+          <Link to="/cart" id="cart-icon">
+            <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+          </Link>
+          <i id="bar" className="fas fa-outdent"></i>
+        </div>
+      </section>
 
-//         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+      <section id="page-header" className="blog-header">
+        <h2>#let's talk</h2>
+        <p>LEAVE A MESSAGE, We love to hear from you!</p>
+      </section>
 
-//         <link rel="stylesheet" href="style.css">
-//     </head>
+      <section id="contact-details" className="section-p1">
+        <div class="details">
+          <span>GET IN TOUCH</span>
+          <h2> Visit one of our agency locations or contact us today</h2>
+          <h3>Head Office</h3>
+          <div>
+            <li>
+              <i class="fal fa-map"></i>
+              <p>56 Glassford Street Glasgow G1 1UL New York</p>
+            </li>
+            <li>
+              <i class="fal fa-envelope"></i>
+              <p>contact@example.com</p>
+            </li>
+            <li>
+              <i class="fal fa-phone-alt"></i>
+              <p>contact@example.com</p>
+            </li>
+            <li>
+              <i class="fal fa-clock"></i>
+              <p>Monday to Saturday: 9.00am to 16.00pm</p>
+            </li>
+          </div>
+        </div>
 
-//     <body>
+        <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.3696783919905!2d112.6823587!3d-7.299989299999992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fc44d5ec05f1%3A0x7994a9cbdfaa118d!2sJl.%20Bintang%20Graha%20Famili%20IV%20No.211%2C%20Babatan%2C%20Kec.%20Wiyung%2C%20Surabaya%2C%20Jawa%20Timur%2060227%2C%20Indonesia!5e0!3m2!1sen!2skr!4v1748869144149!5m2!1sen!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </section>
 
+      <section id="form-details">
+        <form action="">
+          <span>LEAVE A MESSAGE</span>
+          <h2>We love to hear from you</h2>
+          <input type="text" placeholder="Your Name" />
+          <input type="text" placeholder="E-mail" />
+          <input type="text" placeholder="Subject" />
+          <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+          <button>Submit</button>
+        </form>
 
-//         <script src="script.js"></script>
+        <div class="people">
+          <div>
+            <img src="img/people/1.png" alt="" />
+            <p><span>John Doe</span> Senior Marketing Manager <br> Phone: + 000 123 000 77 88</br> <br> Email: contact@example.com</br></p>
+          </div>
+          <div>
+            <img src="img/people/2.png" alt="" />
+            <p><span>William Smith</span> Senior Marketing Manager <br> Phone: + 000 123 000 77 88</br> <br> Email: contact@example.com</br></p>
+          </div>
+          <div>
+            <img src="img/people/3.png" alt="" />
+            <p><span>Emma Stone</span> Senior Marketing Manager <br> Phone: + 000 123 000 77 88</br> <br> Email: contact@example.com</br></p>
+          </div>
+        </div>
+      </section>
 
-//     </body>
+      <section id="newsletter" className="section-p1 section-m1">
+        <div className="newstext">
+          <h4>Sign Up For Newsletters</h4>
+          <p>
+            Get E-Mail Updates About Our Latest Update And
+            <span>Special Offers</span>
+          </p>
+        </div>
+        <div className="form">
+          <input type="text" placeholder="Your E-Mail Address" />
+          <button className="normal">Sign Up</button>
+        </div>
+      </section>
 
-// </html>
+      <footer className="section-p1">
+        <div className="col">
+          <img
+            className="logo"
+            style={{ width: "150px" }}
+            src="/img/logo.png"
+            alt=""
+          />
+          <h4>Contact</h4>
+          <p>
+            <strong>Address: </strong> Sejong University
+          </p>
+          <p>
+            <strong>Phone: </strong> +82 9242 3343 / +62 8345 2423
+          </p>
+          <p>
+            <strong>Hours: </strong>09:00 - 21:00, Monday - Sunday
+          </p>
+          <div className="follow">
+            <h4>Follow us</h4>
+            <div className="icon">
+              <i className="fab fa-facebook"></i>
+              <i className="fab fa-twitter"></i>
+              <i className="fab fa-instagram"></i>
+              <i className="fab fa-pinterest"></i>
+              <i className="fab fa-youtube"></i>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <h4>About</h4>
+          <a href="#">About us</a>
+          <a href="#">Delivery Information</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Contact us</a>
+        </div>
+        <div className="col">
+          <h4>My Account</h4>
+          <a href="#">Sign In</a>
+          <a href="#">View Chart</a>
+          <a href="#">My Wishlist</a>
+          <a href="#">Track My Orders</a>
+          <a href="#">Help</a>
+        </div>
+        <div className="col install">
+          <h4>Install App</h4>
+          <p>From App Store / Google Play</p>
+          <div className="row">
+            <img src="/img/pay/app.jpg" alt="" />
+            <img src="/img/pay/play.jpg" alt="" />
+          </div>
+          <p>Secured Payment Gateways</p>
+          <img src="/img/pay/pay.png" alt="" />
+        </div>
+        <div className="copyright">
+          <p>© 2025. Wong Liyo Ngerti Opo Corporation</p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Contact;
